22462bb8bd71a31fb83e9bd8168a284c74ab522b
TC94615 <TCLee0117@gmail.com>
2015-04-27 00:09:55 +0800

    Add icon Add image picker

--- CollectionViewPractice/OSShareThankViewController.m
+++ CollectionViewPractice/OSShareThankViewController.m
@@ -10,19 +10,102 @@
 static const CGFloat buttonSide = 30;
 static const CGFloat SeparatorLineHeight = 1;
 
+@interface OSShareThankViewController()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property (nonatomic, strong) UIView *toSomeoneYouThanksView;
+@property (nonatomic, strong) UIImageView *selectedImageView;
+@property (nonatomic, strong) UIImagePickerController *imagePickerController;
+@end
+
 @implementation OSShareThankViewController
 - (void) loadView {
     [super loadView];
 
 }
 
+- (void) imagePickerController:(UIImagePickerController *) picker didFinishPickingImage:(UIImage *) image editingInfo:(NSDictionary *) editingInfo {
+    [self.selectedImageView setImage:image];
+    [self.selectedImageView intrinsicContentSize];
+    NSLog(@">>>>>>>>>>>> NSStringFromCGRect(self.selectedImageView) = %@", NSStringFromCGRect(self.selectedImageView.frame));
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
 - (void) viewDidLoad {
     [super viewDidLoad];
-
+    self.view.backgroundColor = [UIColor whiteColor];
     [self configureNavigationController];
     [self configureToSomeoneYouThanksView];
 
 
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    self.imagePickerController = imagePickerController;
+
+    UIImageView *avatarImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ic_person_black_48dp.png"]];
+    avatarImageView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:avatarImageView];
+    [avatarImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(5);
+        make.top.equalTo(self.toSomeoneYouThanksView.mas_bottom).offset(10);
+        make.width.equalTo(@40);
+        make.height.equalTo(@40);
+    }];
+
+    CGFloat circleRadius = 15;
+    UITapGestureRecognizer *loadPhotoButtonBackgroundTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                                          action:@selector(tapLoadPhotoButton:)];
+    UIView *loadPhotoButtonBackgroundView = [[UIView alloc] init];
+    [loadPhotoButtonBackgroundView addGestureRecognizer:loadPhotoButtonBackgroundTapGesture];
+    loadPhotoButtonBackgroundView.layer.cornerRadius = circleRadius;
+    loadPhotoButtonBackgroundView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:loadPhotoButtonBackgroundView];
+    [loadPhotoButtonBackgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(5);
+        make.top.equalTo(avatarImageView.mas_bottom).offset(10);
+        make.width.equalTo(@(circleRadius * 2));
+        make.height.equalTo(@(circleRadius * 2));
+    }];
+
+    UIButton *loadPhotoButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [loadPhotoButton addTarget:self action:@selector(tapLoadPhotoButton:)
+              forControlEvents:UIControlEventTouchUpInside];
+    [loadPhotoButton setImage:[[UIImage imageNamed:@"ic_photo_camera_black_48dp.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]
+                     forState:UIControlStateNormal];
+    loadPhotoButton.tintColor = [UIColor whiteColor];
+    [self.view addSubview:loadPhotoButton];
+    [loadPhotoButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(loadPhotoButtonBackgroundView);
+        make.centerY.equalTo(loadPhotoButtonBackgroundView);
+        make.width.equalTo(@20);
+        make.height.equalTo(@20);
+    }];
+
+    UIImageView *selectedImageView = [[UIImageView alloc] init];
+    selectedImageView.clipsToBounds = YES;
+    selectedImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [self.view addSubview:selectedImageView];
+    [selectedImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(loadPhotoButton.mas_bottom);
+        make.width.equalTo(self.view).offset(-10);
+    }];
+    self.selectedImageView = selectedImageView;
+
+    UIImageView *closeImageView = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"ic_cancel_black_48dp.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    closeImageView.tintColor = [UIColor lightGrayColor];
+    [self.view addSubview:closeImageView];
+    [closeImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.view).offset(-5);
+        make.top.equalTo(self.toSomeoneYouThanksView.mas_bottom);
+        make.width.equalTo(@(circleRadius * 2));
+        make.height.equalTo(@(circleRadius * 2));
+    }];
+}
+
+- (void) tapLoadPhotoButton:(UIButton *) sender {
+    [self presentViewController:self.imagePickerController animated:YES
+                     completion:nil];
 }
 
 - (void) configureToSomeoneYouThanksView {
@@ -35,6 +118,7 @@ static const CGFloat SeparatorLineHeight = 1;
         make.width.equalTo(self.view);
         make.height.equalTo(@(buttonSide));
     }];
+    self.toSomeoneYouThanksView = toSomeoneYouThanksView;
     UILabel *sendThanksLabel = [[UILabel alloc] init];
     sendThanksLabel.text = @"To: ";
     sendThanksLabel.textColor = [UIColor lightGrayColor];
