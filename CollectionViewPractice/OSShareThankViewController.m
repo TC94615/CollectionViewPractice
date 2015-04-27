fe65a31b81f8ed13b5f6114f6068bc553e84ed6f
TC94615 <TCLee0117@gmail.com>
2015-04-27 11:24:11 +0800

    Customize navigation bar button Add button to close loaded image

--- CollectionViewPractice/OSShareThankViewController.m
+++ CollectionViewPractice/OSShareThankViewController.m
@@ -9,12 +9,16 @@
 
 static const CGFloat buttonSide = 30;
 static const CGFloat SeparatorLineHeight = 1;
+static const CGFloat HorizantalPadding = 10;
+static const CGFloat VerticalPadding = 10;
 
 @interface OSShareThankViewController()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (nonatomic, strong) UIView *toSomeoneYouThanksView;
 @property (nonatomic, strong) UIImageView *selectedImageView;
 @property (nonatomic, strong) UIImagePickerController *imagePickerController;
+@property (nonatomic, strong) UIView *loadPhotoButtonBackgroundView;
+@property (nonatomic, strong) UIButton *imageCloseButton;
 @end
 
 @implementation OSShareThankViewController
@@ -24,9 +28,21 @@ static const CGFloat SeparatorLineHeight = 1;
 }
 
 - (void) imagePickerController:(UIImagePickerController *) picker didFinishPickingImage:(UIImage *) image editingInfo:(NSDictionary *) editingInfo {
-    [self.selectedImageView setImage:image];
-    [self.selectedImageView intrinsicContentSize];
-    NSLog(@">>>>>>>>>>>> NSStringFromCGRect(self.selectedImageView) = %@", NSStringFromCGRect(self.selectedImageView.frame));
+    if (image.size.width <= CGRectGetWidth(self.selectedImageView.frame) && image.size.height <= CGRectGetHeight(self.selectedImageView.frame)) {
+        self.selectedImageView.contentMode = UIViewContentModeTopLeft;
+        [self.selectedImageView setImage:image];
+    } else {
+        self.selectedImageView.contentMode = UIViewContentModeScaleAspectFit;
+        CGFloat scaledImageViewHeight = CGRectGetWidth(self.selectedImageView.frame) * image.size.height / image.size.width;
+        [self.selectedImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(self.view);
+            make.top.equalTo(self.loadPhotoButtonBackgroundView.mas_bottom).offset(VerticalPadding);
+            make.width.equalTo(self.view).offset(-HorizantalPadding * 2);
+            make.height.equalTo(@(scaledImageViewHeight));
+        }];
+        [self.selectedImageView setImage:image];
+    }
+    self.imageCloseButton.hidden = NO;
     [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -46,8 +62,8 @@ static const CGFloat SeparatorLineHeight = 1;
     avatarImageView.backgroundColor = [UIColor lightGrayColor];
     [self.view addSubview:avatarImageView];
     [avatarImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view).offset(5);
-        make.top.equalTo(self.toSomeoneYouThanksView.mas_bottom).offset(10);
+        make.left.equalTo(self.view).offset(HorizantalPadding);
+        make.top.equalTo(self.toSomeoneYouThanksView.mas_bottom).offset(VerticalPadding);
         make.width.equalTo(@40);
         make.height.equalTo(@40);
     }];
@@ -61,11 +77,12 @@ static const CGFloat SeparatorLineHeight = 1;
     loadPhotoButtonBackgroundView.backgroundColor = [UIColor lightGrayColor];
     [self.view addSubview:loadPhotoButtonBackgroundView];
     [loadPhotoButtonBackgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view).offset(5);
-        make.top.equalTo(avatarImageView.mas_bottom).offset(10);
+        make.left.equalTo(self.view).offset(HorizantalPadding);
+        make.top.equalTo(avatarImageView.mas_bottom).offset(VerticalPadding);
         make.width.equalTo(@(circleRadius * 2));
         make.height.equalTo(@(circleRadius * 2));
     }];
+    self.loadPhotoButtonBackgroundView = loadPhotoButtonBackgroundView;
 
     UIButton *loadPhotoButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [loadPhotoButton addTarget:self action:@selector(tapLoadPhotoButton:)
@@ -83,23 +100,40 @@ static const CGFloat SeparatorLineHeight = 1;
 
     UIImageView *selectedImageView = [[UIImageView alloc] init];
     selectedImageView.clipsToBounds = YES;
-    selectedImageView.contentMode = UIViewContentModeScaleAspectFill;
+    selectedImageView.contentMode = UIViewContentModeScaleAspectFit;
     [self.view addSubview:selectedImageView];
     [selectedImageView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view);
-        make.top.equalTo(loadPhotoButton.mas_bottom);
-        make.width.equalTo(self.view).offset(-10);
+        make.top.equalTo(loadPhotoButtonBackgroundView.mas_bottom).offset(VerticalPadding);
+        make.width.equalTo(self.view).offset(-HorizantalPadding * 2);
+        make.bottom.equalTo(self.view).offset(-VerticalPadding);
     }];
     self.selectedImageView = selectedImageView;
 
-    UIImageView *closeImageView = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"ic_cancel_black_48dp.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
-    closeImageView.tintColor = [UIColor lightGrayColor];
-    [self.view addSubview:closeImageView];
-    [closeImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self.view).offset(-5);
-        make.top.equalTo(self.toSomeoneYouThanksView.mas_bottom);
+    UIButton *imageCloseButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [imageCloseButton addTarget:self action:@selector(tapCloseButton:) forControlEvents:UIControlEventTouchUpInside];
+    [imageCloseButton setImage:[[UIImage imageNamed:@"ic_cancel_black_48dp.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]
+                      forState:UIControlStateNormal];
+    imageCloseButton.tintColor = [UIColor lightGrayColor];
+    [self.view addSubview:imageCloseButton];
+    [imageCloseButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(selectedImageView.mas_right);
+        make.centerY.equalTo(selectedImageView.mas_top);
         make.width.equalTo(@(circleRadius * 2));
         make.height.equalTo(@(circleRadius * 2));
+    }];
+    imageCloseButton.hidden = YES;
+    self.imageCloseButton = imageCloseButton;
+}
+
+- (void) tapCloseButton:(UIButton *) sender {
+    self.selectedImageView.image = nil;
+    self.imageCloseButton.hidden = YES;
+    [self.selectedImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(self.loadPhotoButtonBackgroundView.mas_bottom).offset(VerticalPadding);
+        make.width.equalTo(self.view).offset(-HorizantalPadding * 2);
+        make.bottom.equalTo(self.view).offset(-VerticalPadding);
     }];
 }
 
@@ -120,7 +154,7 @@ static const CGFloat SeparatorLineHeight = 1;
     }];
     self.toSomeoneYouThanksView = toSomeoneYouThanksView;
     UILabel *sendThanksLabel = [[UILabel alloc] init];
-    sendThanksLabel.text = @"To: ";
+    sendThanksLabel.text = @"致： ";
     sendThanksLabel.textColor = [UIColor lightGrayColor];
     [toSomeoneYouThanksView addSubview:sendThanksLabel];
     [sendThanksLabel mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -164,22 +198,51 @@ static const CGFloat SeparatorLineHeight = 1;
 }
 
 - (void) configureNavigationController {
-    self.navigationItem.title = @"Share Your Thanks";
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel"
-                                                                             style:UIBarButtonItemStylePlain
-                                                                            target:self
-                                                                            action:@selector(tapCancelButton:)];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Public"
-                                                                              style:UIBarButtonItemStylePlain
-                                                                             target:self
-                                                                             action:@selector(tapPublicButton:)];
+    CGFloat buttonFontSize = 11;
+    self.navigationItem.title = @"分享感謝文";
+    [self.navigationController.navigationBar setTitleTextAttributes:
+                                               @{NSFontAttributeName :
+                                                 [UIFont systemFontOfSize:15]}];
+
+    UIView *leftButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 34, 17)];
+    leftButtonView.backgroundColor = [UIColor pageBackgroundColor];
+    leftButtonView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    leftButtonView.layer.borderWidth = 1;
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [leftButton addTarget:self action:@selector(tapCancelButton:) forControlEvents:UIControlEventTouchUpInside];
+    [leftButton setTitle:@"取消" forState:UIControlStateNormal];
+    [leftButton setTintColor:[UIColor pageBackgroundColor]];
+    leftButton.titleLabel.font = [UIFont boldSystemFontOfSize:buttonFontSize];
+    [leftButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [leftButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateHighlighted];
+    leftButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [leftButtonView addSubview:leftButton];
+    [leftButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(leftButtonView);
+    }];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftButtonView];
+
+    UIView *rightButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 34, 17)];
+    rightButtonView.backgroundColor = [UIColor redColor];
+    UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [rightButton addTarget:self action:@selector(tapPublicButton:) forControlEvents:UIControlEventTouchUpInside];
+    [rightButton setTitle:@"發布" forState:UIControlStateNormal];
+    rightButton.titleLabel.font = [UIFont boldSystemFontOfSize:buttonFontSize];
+    [rightButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [rightButton setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+    rightButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [rightButtonView addSubview:rightButton];
+    [rightButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(rightButtonView);
+    }];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightButtonView];
 }
 
-- (void) tapCancelButton:(UIBarButtonItem *) sender {
+- (void) tapCancelButton:(UIButton *) sender {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void) tapPublicButton:(UIBarButtonItem *) sender {
+- (void) tapPublicButton:(UIButton *) sender {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
