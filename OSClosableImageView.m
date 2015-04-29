9de4aca840c387b96af0c33f91aa541a049c17ed
TC94615 <TCLee0117@gmail.com>
2015-04-29 17:22:43 +0800

    Refactor

---
+++ OSClosableImageView.m
@@ -0,0 +1,12 @@
+//
+// Created by 李道政 on 15/4/29.
+// Copyright (c) 2015 oSolve. All rights reserved.
+//
+
+#import "OSClosableImageView.h"
+
+
+@implementation OSClosableImageView {
+
+}
+@end
---
+++ UIDImen.h
@@ -0,0 +1,10 @@
+//
+// Created by 李道政 on 15/4/29.
+// Copyright (c) 2015 oSolve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIDImen : NSObject
+@end
